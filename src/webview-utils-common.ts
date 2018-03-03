fa8667963e10418ec2dd183e30826c650857d485
EddyVerbruggen <eddyverbruggen@gmail.com>
2018-03-03 17:57:33 +0100

    #5 How to trigger webview loadStatedEvent after insert custom header (add eventhandler functions)

---
+++ src/webview-utils-common.ts
@@ -0,0 +1,26 @@
+import { LoadEventData, NavigationType } from "tns-core-modules/ui/web-view";
+import { Observable } from "tns-core-modules/ui/core/bindable";
+
+export function onLoadStarted(observable: Observable, url: string, navigationType: NavigationType) {
+  let args = <LoadEventData>{
+    eventName: "loadStarted",
+    object: this,
+    url: url,
+    navigationType: navigationType,
+    error: undefined
+  };
+
+  observable.notify(args);
+}
+
+export function onLoadFinished(observable: Observable, url: string, error?: string) {
+  let args = <LoadEventData>{
+    eventName: "loadFinished",
+    object: observable,
+    url: url,
+    navigationType: undefined,
+    error: error
+  };
+
+  observable.notify(args);
+}
